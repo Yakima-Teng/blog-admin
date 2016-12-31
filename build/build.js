173653c055078332b4b443fadb4c859dfbf8f52c
Yakima-Teng <cleveryun@163.com>
2016-12-31 23:37:59 +0800

    考虑到做为脚手架的原因，不在npm run build时发布项目到github pages，而是在package.json中专门添加一个npm run deploy命令

--- build/build.js
+++ build/build.js
@@ -8,7 +8,7 @@ var config = require('../config')
 var ora = require('ora')
 var webpack = require('webpack')
 var webpackConfig = require('./webpack.prod.conf')
-var ghpages = require('gh-pages')
+// var ghpages = require('gh-pages')
 
 console.log(
   '  Tip:\n' +
@@ -24,28 +24,28 @@ rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
 
-function deploy () {
-  spinner = ora('Start deploying')
-  spinner.start()
-  ghpages.publish(path.resolve(__dirname, '../dist'), {
-    src: [
-      '**/*'
-    ]
-  }, function (err) {
-    spinner.stop()
-    if (err) {
-      console.log(err)
-      console.log('Failed deploying')
-      return
-    }
-    console.log('Finished deploying')
-  })
-}
+// function deploy () {
+//   spinner = ora('Start deploying')
+//   spinner.start()
+//   ghpages.publish(path.resolve(__dirname, '../dist'), {
+//     src: [
+//       '**/*'
+//     ]
+//   }, function (err) {
+//     spinner.stop()
+//     if (err) {
+//       console.log(err)
+//       console.log('Failed deploying')
+//       return
+//     }
+//     console.log('Finished deploying')
+//   })
+// }
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
   if (err) throw err
-  deploy()
+  // deploy()
   process.stdout.write(stats.toString({
     colors: true,
     modules: false,
