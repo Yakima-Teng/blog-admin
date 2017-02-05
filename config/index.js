4ec2d262ed5e35bacc0a140c044f37fdf3718e4e
Yakima-Teng <cleveryun@163.com>
2017-02-05 16:07:36 +0800

    revise url path to /blog-admin/ from /demos/

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/demos/',
+    assetsPublicPath: '/blog-admin/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -20,7 +20,7 @@ module.exports = {
     env: require('./dev.env'),
     port: 8080,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/demos/',
+    assetsPublicPath: '/blog-admin/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
